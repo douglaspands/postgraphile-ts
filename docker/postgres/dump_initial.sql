9bf1c966594e3dc40594a95dfcb4bb307c80597c
Douglas Panhota <douglaspands@gmail.com>
2023-02-01 00:37:49 -0300

    ajuste nos grants

--- docker/postgres/dump_initial.sql
+++ docker/postgres/dump_initial.sql
@@ -4,16 +4,22 @@ CREATE USER app_user WITH PASSWORD '7mF0uXv82qy8' INHERIT;
 -- roles
 CREATE ROLE read_role WITH NOINHERIT;
 CREATE ROLE write_role WITH NOINHERIT;
+CREATE ROLE admin_role WITH NOINHERIT CREATEROLE;
 
 -- groups
 CREATE ROLE admin_group WITH INHERIT;
 CREATE ROLE read_group WITH INHERIT;
+CREATE ROLE write_group WITH INHERIT;
 
 -- grants
-GRANT read_role, write_role to admin_group;
+GRANT admin_role, read_role, write_role to admin_group;
 GRANT read_role to read_group;
-GRANT admin_group to app_user;
+GRANT write_role to write_group;
 
 -- databases
-CREATE DATABASE postgraphile_shadow WITH OWNER app_user ENCODING UTF8 TABLESPACE pg_default;
 CREATE DATABASE postgraphile WITH OWNER app_user ENCODING UTF8 TABLESPACE pg_default;
+CREATE DATABASE postgraphile_shadow WITH OWNER app_user ENCODING UTF8 TABLESPACE pg_default;
+
+-- grant database
+GRANT ALL ON DATABASE postgraphile TO admin_role;
+GRANT ALL ON DATABASE postgraphile_shadow TO admin_role;
--- migrations/grants.sql
+++ migrations/grants.sql
@@ -1,12 +1,10 @@
 -- permissions
-GRANT USAGE ON SCHEMA app_public to read_role;
-GRANT USAGE ON SCHEMA app_public to write_role;
-GRANT USAGE ON SCHEMA app_public to app_user;
-GRANT USAGE ON TYPE app_public.jwt_token TO read_role;
-GRANT ALL ON TYPE app_public.jwt_token TO write_role;
+GRANT USAGE ON SCHEMA app_public to read_role, write_role;
+GRANT ALL ON SCHEMA app_public to admin_role;
+
 GRANT SELECT ON ALL TABLES IN SCHEMA app_public TO read_role;
-GRANT ALL ON ALL TABLES IN SCHEMA app_public TO write_role;
-GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA app_public TO read_role;
-GRANT ALL ON ALL SEQUENCES IN SCHEMA app_public TO write_role;
-GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA app_public TO read_role;
-GRANT ALL ON ALL FUNCTIONS IN SCHEMA app_public TO write_role;
+
+GRANT ALL ON TYPE app_public.jwt_token TO read_role, write_role, admin_role;
+GRANT ALL ON ALL TABLES IN SCHEMA app_public TO write_role, admin_role;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA app_public TO read_role, write_role, admin_role;
+GRANT ALL ON ALL FUNCTIONS IN SCHEMA app_public TO read_role, write_role, admin_role;
